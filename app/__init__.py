b524657315cc87e1e5a15f21d6a6f6a9bb0ddc0e
230055Fawwaz <evan.areefa@gmail.com>
2026-04-25 08:42:37 +0700

    chore: add library list, flask initialization and entry point

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,9 @@
 # Tanggal:   25-04-2026
 # ==========================================
 
+from flask import Flask
+
+app = Flask(__name__)
+
+# Import routes di bagian bawah untuk menghindari circular import
+from app import routes
--- run.py
+++ run.py
@@ -5,3 +5,8 @@
 # Tanggal:   25-04-2026
 # ==========================================
 
+from app import app
+
+if __name__ == "__main__":
+    # Menjalankan aplikasi dalam mode debug
+    app.run(debug=True)

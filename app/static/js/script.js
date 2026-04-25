d1e6a8b8410f839dd80c4af220b143a8d5a82c02
230055Fawwaz <evan.areefa@gmail.com>
2026-04-25 14:49:52 +0700

    feat: add base code for web pages

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -5,3 +5,11 @@
 // Tanggal:   25-04-2026
 // ==========================================
 
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleBtn = document.getElementById('menu-toggle');
+    const sidebar = document.getElementById('sidebar');
+
+    toggleBtn.addEventListener('click', function() {
+        sidebar.classList.toggle('collapsed');
+    });
+});

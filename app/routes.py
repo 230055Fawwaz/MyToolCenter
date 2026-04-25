0043121b75c2751e9dd32422f5251fad20532bd4
230055Fawwaz <evan.areefa@gmail.com>
2026-04-25 14:49:33 +0700

    feat: add routes for web pages

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,14 @@
 # Tanggal:   25-04-2026
 # ==========================================
 
+from flask import render_template
+from app import app  # Mengimpor instance app dari package 'app'
+
+@app.route('/')
+@app.route('/dashboard')
+def dashboard():
+    return render_template('index.html')
+
+@app.route('/scripts')
+def scripts():
+    return render_template('scripts.html')

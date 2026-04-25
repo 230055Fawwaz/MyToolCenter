8623fd4cb5c8340b0b393be8613dd06878d57897
230055Fawwaz <evan.areefa@gmail.com>
2026-04-25 08:15:43 +0700

    initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+# ==========================================
+# Nama File: test_app.py
+# Deskripsi: Tes aplikasi
+# Penulis:   Fawwaz Yaqzhan
+# Tanggal:   25-04-2026
+# ==========================================
+
